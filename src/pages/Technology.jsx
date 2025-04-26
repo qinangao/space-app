2ec8e7ee9d5e6fb6820c469888b55cba8964e190
qinanagao@gmail.com <qinanagao@gmail.com>
2025-04-26 11:42:09 +0100

    structure and style tech page

--- src/pages/Technology.jsx
+++ src/pages/Technology.jsx
@@ -1,24 +1,42 @@
 import NavBar from "../components/NavBar";
 import "../styles/Technology.scss";
 
-function Technology({ technology }) {
+function Technology({ technology, activeIndex, setActiveIndex }) {
   return (
     <div className="page techpage">
       <NavBar />
       <div className="main">
-        <h1 className="title">
-          {" "}
-          <span>03</span>Space launch 101{" "}
-        </h1>
-        <p>The terminology... </p>
+        <section className="container">
+          <div>
+            <h1 className="title">
+              <span>03</span>Space launch 101
+            </h1>
+            <div className="tech-left">
+              <div className="tect-nav">
+                {technology.map((tech, index) => (
+                  <div
+                    key={index}
+                    className={`button ${
+                      index === activeIndex ? "active-button" : ""
+                    }`}
+                    onClick={() => setActiveIndex(index)}
+                  >
+                    {index + 1}
+                  </div>
+                ))}
+              </div>
+              <div className="tech-textbox">
+                <p className="intro">The terminology... </p>
+                <h2 className="name">{technology[activeIndex].name}</h2>
+                <p className="content">{technology[activeIndex].description}</p>
+              </div>
+            </div>
+          </div>
 
-        {technology.map((tech, index) => (
-          <section key={index}>
-            <h1>{tech.name}</h1>
-            <p>{tech.description}</p>
-            <img src={tech.images.portrait} alt={tech.name} />
-          </section>
-        ))}
+          <div className="imgbox">
+            <img src={technology[activeIndex].images.portrait} alt="" />
+          </div>
+        </section>
       </div>
     </div>
   );
