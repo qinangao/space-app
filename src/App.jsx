49293715413ab0b81b73636657eafedc7b26b426
qinanagao@gmail.com <qinanagao@gmail.com>
2025-04-26 11:40:32 +0100

    refactor index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,50 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useState } from "react";
 import Home from "./pages/Home";
 import Destination from "./pages/Destination";
 import Crew from "./pages/Crew";
 import Technology from "./pages/Technology";
 import data from "./data/data.json";
-import NavBar from "./components/NavBar";
 
 const destination = data.destinations;
 const crew = data.crew;
 const technology = data.technology;
 
 function App() {
+  const [activeIndex, setActiveIndex] = useState(0);
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route
           path="destination"
-          element={<Destination destination={destination} />}
+          element={
+            <Destination
+              destination={destination}
+              activeIndex={activeIndex}
+              setActiveIndex={setActiveIndex}
+            />
+          }
         />
-        <Route path="crew" element={<Crew crew={crew} />} />
+        <Route
+          path="crew"
+          element={
+            <Crew
+              crew={crew}
+              activeIndex={activeIndex}
+              setActiveIndex={setActiveIndex}
+            />
+          }
+        />
         <Route
           path="technology"
-          element={<Technology technology={technology} />}
+          element={
+            <Technology
+              technology={technology}
+              activeIndex={activeIndex}
+              setActiveIndex={setActiveIndex}
+            />
+          }
         />
       </Routes>
     </BrowserRouter>
