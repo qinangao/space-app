8251b3909ba2c2ebbb92331a7b4d1dfa6874d4e4
qinanagao@gmail.com <qinanagao@gmail.com>
2025-04-26 18:10:42 +0100

    make nav resposive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,26 +1,76 @@
 import { NavLink, Link } from "react-router-dom";
 import "../styles/NavBar.scss";
+import { useState } from "react";
 function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <nav className="navbar">
       <Link className="logo" to="/">
         <img src="/assets/shared/logo.svg" alt="logo" />
       </Link>
       <hr className="divider" />
-      <ul>
-        <li>
-          <NavLink to="/">00 HOME</NavLink>
-        </li>
-        <li>
-          <NavLink to="/destination">01 DESTINATION</NavLink>
-        </li>
-        <li>
-          <NavLink to="/crew">02 CREW</NavLink>
-        </li>
-        <li>
-          <NavLink to="/technology">03 TECHNOLOGY</NavLink>
-        </li>
-      </ul>
+      <div className="desknav">
+        <ul>
+          <li>
+            <NavLink to="/">
+              <span>00</span> HOME
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/destination">
+              <span>01</span> DESTINATION
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/crew">
+              <span>02</span> CREW
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/technology">
+              <span>03</span> TECHNOLOGY
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+
+      {isOpen && (
+        <div className="mobilenav">
+          <ul>
+            <li>
+              <NavLink to="/">
+                <span>00</span> HOME
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/destination">
+                <span>01</span> DESTINATION
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/crew">
+                <span>02</span> CREW
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/technology">
+                <span>03</span> TECHNOLOGY
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      )}
+
+      <div className="hamburger-btn" onClick={() => setIsOpen(!isOpen)}>
+        <img
+          src={
+            isOpen
+              ? "/assets/shared/icon-close.svg"
+              : "/assets/shared/icon-hamburger.svg"
+          }
+          alt="open-btn"
+        />
+      </div>
     </nav>
   );
 }
